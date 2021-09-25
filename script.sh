ac64b512c3748ba9615c968efde53190d337dfbf
Parmicciano <85128225+Parmicciano@users.noreply.github.com>
2021-09-25 15:25:00 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,25 +1,24 @@
-
-
-cd /var/www/
-TOKEN=YOURTOKEN
-cd html
-
+#!/bin/bash
+TOKEN=
+REPONAME=  
+cd /var/www/ 
+cd  html
 checkthebranch() {
-if [[ $(git remote show origin) != *"up to date"* ]]; then
-  cd ../ &&  echo "downloading" &&  sudo git clone https://Parmicciano:"${TOKEN}"@github.com/Parmicciano/REPONAME && sudo rm -r html && sudo mv REPONAME html
+if [[ $(git remote show origin) == *"out of date"* ]]; then
+cd /var/www &&  echo "downloading" &&  sudo git clone https://Parmicciano:"${TOKEN}"@github.com/Parmicciano/"${REPONAME}" && sudo rm -r html && sudo mv "${REPONAME}" html
 else
 echo "already up to date"
 fi
 }
 
-checkthebranch()
+checkthebranch
 sleep 10
-checkthebranch()
+checkthebranch
 sleep 10
-checkthebranch()
+checkthebranch
 sleep 10
-checkthebranch()
+checkthebranch
 sleep 10
-checkthebranch()
+checkthebranch
 sleep 10
-checkthebranch()
+checkthebranch
