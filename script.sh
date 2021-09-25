919795dd2c30da449412ae0d59053932c467cd7b
Parmicciano <85128225+Parmicciano@users.noreply.github.com>
2021-09-25 14:51:30 +0200

    Code check every ten second> cron has to be 1 m

--- script.sh
+++ script.sh
@@ -1,9 +1,25 @@
+
+
 cd /var/www/
 TOKEN=YOURTOKEN
 cd html
+
+checkthebranch() {
 if [[ $(git remote show origin) != *"up to date"* ]]; then
   cd ../ &&  echo "downloading" &&  sudo git clone https://Parmicciano:"${TOKEN}"@github.com/Parmicciano/REPONAME && sudo rm -r html && sudo mv REPONAME html
 else
 echo "already up to date"
-
 fi
+}
+
+checkthebranch()
+sleep 10
+checkthebranch()
+sleep 10
+checkthebranch()
+sleep 10
+checkthebranch()
+sleep 10
+checkthebranch()
+sleep 10
+checkthebranch()
